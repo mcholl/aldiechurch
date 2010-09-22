5da635b6ba994b48c23ddc769a6b2c9d1d3c077d
Michael Hollinger <mcholl@yahoo.com>
2010-09-22 05:52:38 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,54 @@
+<STYLE type='text/css'>
+	#header {
+		font-family: "Callibri", sans-serif;
+		font-size: 10pt;
+		color: rgb(66, 120, 211);
+	}
+	
+	#header p {
+		position: relative;
+		margin-top: 0px;
+	
+	.login_req {
+		color: rgb(180, 180, 180);
+	}
+</STYLE>
+
+<DIV id='header'>
+	<IMG src='RedeemerLogo.png' />
+	<p>Ears to Hear, Hearts to Love, and Wills devoted to Serve</p>
+</DIV>
+<DIV id='nav'>
+	<ul>
+		<li>Home</li>
+		<li>Sermons</li>
+		<li>About Us
+			<ul>
+				<li>Worship Schedule</li>
+				<li>Directions</li>
+				<li>What We Believe</li>
+				<li>Staff</li>
+				<li>Frequently Asked Questions</li>
+			</ul>
+		</li>
+		<li>Community
+			<ul>
+				<li>Announcements</li>
+				<li>Calendar</li>
+				<li>Newsletter</li>
+				<li class='login_req'>Prayer List</li>
+				<li class='login_req'>Pictures</li>
+				<li class='login_req'>Facebook Group</li>
+				<li class='login_req'>Member Directory</li>
+			</ul>
+		</li>
+		<li>Wider Church
+			<ul>
+				<li><a href='http://thediocese.net'>The Diocese of Virginia</a></li>
+				<li><a href='http://textweek.com'>The Lectionary</a></li>
+				<li><a href='http://dailyoffice.org'>Daily Offices</a></li>
+				<li>Bible Study Links we like</li>
+			</ul>
+		</li>
+	</ul>
+</DIV>
---
+++ nav.php
@@ -0,0 +1,32 @@
+
+<DIV id='nav'>
+	<ul>
+		<li>Home
+			<ul>			
+				<li>Worship Schedule</li>
+				<li>Directions</li>
+				<li>About Us</li>
+			</ul>
+		</li>
+		<li>Sermons
+		</li>
+		<li>Community
+			<ul>
+				<li>Announcements</li>
+				<li>Calendar</li>
+				<li>Newsletter</li>
+				<li>Prayer List</li>
+				<li>Pictures</li>
+				<li>Facebook Group</li>
+			</ul>
+		</li>
+		<li>Wider Church
+			<ul>
+				<li><a href='http://thediocese.net'>The Diocese of Virginia</a></li>
+				<li><a href='http://textweek.com'>The Lectionary</a></li>
+				<li><a href='http://dailyoffice.org'>Daily Offices</a></li>
+				<li>Bible Study Links we like</li>
+			</ul>
+		</li>
+	</ul>
+</DIV>
