0691aed73ea5152ea65156f85bce3e27742e5c1a
Michael Hollinger <mcholl@yahoo.com>
2010-09-26 19:55:05 -0400

    Post demo

---
+++ wordpress/wp-content/themes/twentyten/header.php
@@ -0,0 +1,49 @@
+<STYLE type='text/css'>
+	#header {
+		background-color: white;
+		font-family: "Callibri", sans-serif;
+		display: block;
+		width: 650px;
+		border: 1px solid black;
+		border-bottom-width: 0px;
+		margin: 0px auto;
+		height: 60px;
+	}
+	/* 		float: left;
+	*/
+	#header IMG, #header DIV { 
+		height: 50px; 		
+		float: left;
+		margin: 5px 25px 5px 25px;
+	}
+	#header p span{
+		font-variant: small-caps;
+		font-size: 20pt; 
+	}
+	#header p span.blue { color: rgb(66, 120, 211); }
+	#header p span.red { color: rgb(214, 0, 29); }
+
+	#header p {		
+		font-size: 11pt;
+		color: rgb(66, 120, 211);
+		margin: 0px;
+		text-align: center;
+	}
+
+	.login_req {
+		color: rgb(180, 180, 180);
+	}
+	
+</STYLE>
+
+<DIV id='header'>
+	<IMG src='images/episshield.png' />
+	<DIV>
+		<p><SPAN class='blue'>The Church of&nbsp;</SPAN><SPAN class='red'>Our Redeemer</SPAN><p>
+		<p>Ears to Hear&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Hearts to Love&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Wills devoted to Serve</p>
+	</DIV>
+	<IMG src='images/large_church2.jpg' />
+	<br />
+</DIV>
+
+<?php include('nav.php'); ?>
