ff64a1fa3b80dfbe6676472f0b2004caabb21262
Michael Hollinger <mcholl@yahoo.com>
2010-10-15 12:53:42 -0400

    Cleaned up Menu.php

--- wordpress/wp-content/themes/twentyten/footer.php
+++
@@ -1,8 +0,0 @@
-		<DIV id='footer'>
-			http://aldiechurch.com  &nbsp;&#164;&nbsp; 39518 John Mosby Highway, Aldie, VA 20105 &nbsp;&#164;&nbsp; (703) 327-4060
-		</DIV>
-		<DIV id='bottommargin'>&nbsp;</DIV>
-
-	</BODY>
-
-</HTML>
